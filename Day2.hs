2d41c4adcae2c7d7cbca158f284721911015668f
Penny Chase <mpchase@mac.com>
2020-11-23 15:24:00 -0500

    Cleaned up Day2 to use part 2 terminology, use monads to clean up code, and do part 2

--- Day2.hs
+++ Day2.hs
@@ -9,7 +9,7 @@ splitComma :: String -> [String]
 splitComma xs = words $ map (\x -> if x == ',' then ' ' else x) xs
 
 data IntcodeComputer = IntcodeComputer
-    { current :: Int
+    { instrPtr :: Int
     , memory :: M.Map Int Int
     }
     deriving (Show)
@@ -28,32 +28,36 @@ data Instruction = Instruction
 mkComputer :: Int -> [Int] -> IntcodeComputer
 mkComputer pos instrs = IntcodeComputer pos (M.fromList (zip [0..] instrs))
 
+initComputer :: IntcodeComputer -> Int -> Int -> IntcodeComputer
+initComputer computer noun verb =
+    computer { memory = M.insert 2 verb $ memory (computer { memory = M.insert 1 noun (memory computer)})}
+
+
 getOpcode :: IntcodeComputer -> Maybe Opcode
 getOpcode computer =
-    case M.lookup (current computer) (memory computer) of
+    case M.lookup (instrPtr computer) (memory computer) of
         Nothing -> Nothing
         Just 99 -> Just Halt
         Just 1  -> Just Add
         Just 2  -> Just Mult
         Just _  -> Nothing
 
+relativeAddr :: IntcodeComputer -> Int -> Maybe Int
+relativeAddr computer offset = M.lookup (instrPtr computer + offset) (memory computer)
 
-getMemContents :: IntcodeComputer -> Int -> Maybe Int
-getMemContents computer n = 
-    case M.lookup (current computer + n) (memory computer) of
-        Nothing -> Nothing
-        (Just addr) -> M.lookup addr (memory computer)
+directAddr :: IntcodeComputer -> Int -> Maybe Int
+directAddr computer addr = M.lookup addr (memory computer)
 
+-- Instruction args:
+-- arg1 and arg2 are indirect addresses: arg is relative address to instruction pointer and the contents
+-- of that address is the address where the actual parameter is located
+-- arg3 is a relative address and the contents is the address for the result
 makeInstruction :: IntcodeComputer -> Maybe Instruction
 makeInstruction computer = do
-    opcode <- getOpcode computer
-    if opcode == Halt
-        then return $ Instruction Halt (-1) (-1) (-1)
-        else do
-            arg1 <- getMemContents computer 1
-            arg2 <- getMemContents computer 2
-            res <-  M.lookup (current computer + 3) (memory computer)
-            return $ Instruction opcode arg1 arg2 res
+    Instruction <$> getOpcode computer 
+                <*> (relativeAddr computer 1 >>= directAddr computer)
+                <*> (relativeAddr computer 2 >>= directAddr computer)
+                <*> relativeAddr computer 3
 
 execInstruction :: IntcodeComputer -> Maybe Instruction -> Maybe (Opcode, IntcodeComputer)
 execInstruction computer instr =
@@ -62,28 +66,44 @@ execInstruction computer instr =
         Just i ->
             case op i of
                 Halt -> Just (Halt, computer)
-                Add  -> Just (Add, computer { memory = M.insert (resLoc i) ((arg1 i) + (arg2 i)) (memory computer) })
-                Mult -> Just (Mult, computer { memory = M.insert (resLoc i) ((arg1 i) * (arg2 i)) (memory computer) })
+                Add  -> Just (Add, computer { memory = M.insert (resLoc i) (arg1 i + arg2 i) (memory computer) })
+                Mult -> Just (Mult, computer { memory = M.insert (resLoc i) (arg1 i * arg2 i) (memory computer) })
 
 runCode :: IntcodeComputer -> Maybe IntcodeComputer
 runCode computer = do
-    let result = execInstruction computer (makeInstruction computer)
-    case result of
+    case execInstruction computer (makeInstruction computer) of
         Nothing -> Nothing
         Just (Halt, computer') -> Just computer'
-        Just (_, computer') -> runCode computer' { current = current computer' + 4 }
+        Just (_, computer') -> runCode computer' { instrPtr = instrPtr computer' + 4 }
+
+checkForValue :: [Int] -> Int -> [(Int, Int)] -> Maybe (Int, Int)
+checkForValue _ _ [] = Nothing
+checkForValue ops val ((n,v):xs) = 
+    case runCode $ initComputer (mkComputer 0 ops) n v of
+        Nothing -> checkForValue ops val xs
+        Just computer' -> compareResult computer'
+    where
+        compareResult c = if directAddr c 0 == Just val then Just (n, v) else checkForValue ops val xs
 
 main :: IO ()
 main = do
     contents <- readFile "day-2-input.txt"
     let ops = map readInt . splitComma $ contents
-    let computer = mkComputer 0 ops
-    let computer' = computer { memory = M.insert 1 12 (memory computer) }
-    let computer'' = computer' { memory = M.insert 2 2 (memory computer') }
-    let c = runCode computer''
+    let computer = initComputer (mkComputer 0 ops) 12 2
+    let c = runCode computer
     case c of
         Nothing -> putStrLn "Bad input"
         Just c' -> putStrLn $ "Result is " ++ show (M.lookup 0 (memory c'))
+
+main2 :: IO ()
+main2 = do
+    contents <- readFile "day-2-input.txt"
+    let ops = map readInt . splitComma $ contents
+    let inputs = [(n,v) | n <- [0..99], v <- [0..99]]
+    case checkForValue ops 19690720 inputs of
+        Nothing -> putStrLn "Not found"
+        Just (noun, verb) -> putStrLn $ "100 * noun + verb = " ++ show (100 * noun + verb)
+
 
 
 
